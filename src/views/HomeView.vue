8382b8246548dc2e3a623d8835cb232a7a605452
MemCh1kk <memetov.k.i.23@gmail.com>
2025-03-27 16:35:32 +0300

    task 5.2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" xs="12">
+        <v-carousel>
+          <v-carousel-item cover></v-carousel-item>
+        </v-carousel>
+      </v-col>
+    </v-row>
+  </v-container>
+
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" xs="12">
+        <v-card>
+          <v-img height="200px"></v-img>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb0">Здесь будет заголовок</h3>
+              <div>Здесь будет описание</div>
+            </div>
+          </v-card-title>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn text>Open</v-btn>
+            <v-btn raised color="primary">Buy</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+
 
 <script>
 // @ is an alias to /src
