61c946be46fdeff0aad40a118888ffbc1cc4cd53
MemCh1kk <memetov.k.i.23@gmail.com>
2025-03-27 10:50:34 +0300

    task 3.1

--- src/App.vue
+++ src/App.vue
@@ -13,19 +13,20 @@
       </v-list-item>
       <v-divider></v-divider>
       <v-list dense>
-        <v-list-item v-for="link in links" :key="link.title">
+        <v-list-item v-for="link in links" :key="link.title" :to="link.url">
           <template v-slot:prepend>
             <v-icon :icon="link.icon"></v-icon>
           </template>
           <v-list-item-title>{{ link.title }}</v-list-item-title>
         </v-list-item>
       </v-list>
+
     </v-navigation-drawer>
     <v-app-bar app dark color="primary">
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
         <v-spacer></v-spacer>
         <v-toolbar-items class="hidden-sm-and-down">
-          <v-btn text v-for="link in links" :key="link.title">
+          <v-btn v-for="link in links" :key="link.title" :to="link.url">
             <v-icon start :icon="link.icon"></v-icon>
             {{ link.title }}
           </v-btn>
@@ -55,6 +56,12 @@
           },
           { title: "New ad", icon: "mdi-note-plus-outline", url: "/new" },
           { title: "My ads", icon: "mdi-view-list-outline", url: "/list" },
+          { title: 'Главная', url: '/', icon: 'mdi-home' },
+          { title: 'Объявления', url: '/list', icon: 'mdi-view-list' },
+          { title: 'Новое объявление', url: '/new', icon: 'mdi-plus' },
+          { title: 'Вход', url: '/login', icon: 'mdi-login' },
+          { title: 'Регистрация', url: '/registration', icon: 'mdi-account' },
+          { title: 'Заказы', url: '/orders', icon: 'mdi-shopping' }
         ],
       };
     },
