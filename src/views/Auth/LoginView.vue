06d2a4252799bbf0a308d41e4207d02a4b09ac9b
MemCh1kk <memetov.k.i.23@gmail.com>
2025-03-27 11:24:32 +0300

    task 3.2

--- src/views/Auth/LoginView.vue
+++ src/views/Auth/LoginView.vue
@@ -13,12 +13,14 @@
                 name="email"
                 label="Email"
                 type="email"
+                v-model="email"
               ></v-text-field>
               <v-text-field
                 prepend-icon="mdi-lock"
                 name="password"
                 label="Password"
                 type="password"
+                v-model="password"
               ></v-text-field>
             </v-form>
           </v-card-text>
@@ -35,7 +37,10 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      email: "",
+      password: ""
+    };
   }
 };
 </script>
